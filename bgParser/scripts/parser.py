791d6d1d5a6af2c111bc1f4236ce13e73a43bff5
Mikhail Tabakaev <47657224+Mihakgma@users.noreply.github.com>
2024-04-18 07:19:32 +0700

    some changes and refactor moments...

--- bgParser/scripts/parser.py
+++ bgParser/scripts/parser.py
@@ -8,6 +8,8 @@ from os import chdir as os_chdir
 from json import load as json_load
 from re import findall as re_findall
 
+from datetime import datetime
+
 from selenium.common.exceptions import TimeoutException, ElementClickInterceptedException, \
     ElementNotInteractableException, StaleElementReferenceException
 
@@ -19,16 +21,11 @@ with open("..\\resources\\dict.json") as json_file:
     json_data = json_load(json_file)
 
 
-
-
 class WebParser:
-    exclude_marks_web_pages = [
-        "/news/",
-        "-blog-"
-    ]
 
-    def __init__(self, sitemap_address: str):
+    def __init__(self, sitemap_address: str, exclude_marks_web_pages: list):
         self.sitemap_address = sitemap_address
+        self.exclude_marks_web_pages = exclude_marks_web_pages
 
     def start_parsing(self,
                       txt_filename: str,
@@ -40,11 +37,11 @@ class WebParser:
             pg_source = driver.page_source
             print("Количество символов в тексте страницы составило:")
             print(len(pg_source))
-            found_games_htmls = self.find_tag_values(web_page_source=pg_source,
+            found_pages_htmls = self.find_tag_values(web_page_source=pg_source,
                                                      open_tag=open_tag,
                                                      close_tag=close_tag)
-            print(*found_games_htmls, sep='\n')
-            self.write_list_to_txt_file(lst=found_games_htmls,
+            print(*found_pages_htmls, sep='\n')
+            self.write_list_to_txt_file(lst=found_pages_htmls,
                                         filename=txt_filename)
             input("Для закрытия браузера нажмите Enter!")
 
@@ -61,16 +58,24 @@ class WebParser:
         return result
 
     def write_list_to_txt_file(self, lst: list, filename: str):
-        if type(filename) != str or not filename.endswith(".txt"):
-            print("передали неверный агрумент имени файла!")
-            return
+        file_format = ".txt"
+        if type(filename) != str or not filename.endswith(file_format):
+            raise TypeError("Передан неверный аргумент имени файла!")
         with open(filename, 'w') as f:
             for line in lst:
                 f.write(f"{line}\n")
+
+    def get_now_timestamp(self, underspaces: bool=True):
+        if type(underspaces) != bool:
+            raise TypeError("Передан неверный аргумент!")
+        now = datetime.now()
+        time = now.strftime("%d:%m:%Y:%H:%M")
+        return [time, time.replace(":", "_")][underspaces]
 
 
 if __name__ == '__main__':
     print(type(json_data))
     print(json_data)
-    parser_1 = WebParser(sitemap_address=json_data['sitemap_1'])
+    parser_1 = WebParser(sitemap_address=json_data['sitemap_1'],
+                         exclude_marks_web_pages=json_data["exclude_marks_webpages_1"])
     parser_1.start_parsing(txt_filename=json_data['htmls_txt_filename'])
