9862259afbf377cb93b69b73373d79ca8130eca7
Mikhail Tabakaev <47657224+Mihakgma@users.noreply.github.com>
2024-04-25 14:58:43 +0700

    commented code in 1 module has been deleted

--- bgParser/scripts/page_parsing.py
+++ bgParser/scripts/page_parsing.py
@@ -9,32 +9,6 @@ from selenium.webdriver.chrome.service import Service as ChromiumService
 from json import load as json_load
 
 
-# from selenium.webdriver.chrome.options import Options
-# from selenium.webdriver.common.keys import Keys
-# from selenium.webdriver.common.action_chains import ActionChains
-#
-# from pandas import DataFrame, read_html, concat, ExcelFile, ExcelWriter, Series
-# from pandas import isna as pd_isna
-# from pandas import merge as pd_merge
-# from math import isnan as math_isnan
-#
-# from random import uniform as random_uniform
-# from random import randint as random_randint
-# from time import sleep as time_sleep
-#
-# from datetime import datetime as dt
-#
-# from win32clipboard import OpenClipboard, EmptyClipboard, SetClipboardText, CF_UNICODETEXT, CloseClipboard
-# from pyautogui import hotkey as pyt_hotkey
-#
-# from re import findall as re_findall
-# from re import search as re_search
-#
-# from tqdm.notebook import tqdm
-#
-# from shutil import copyfile as shutil_copyfile
-
-
 def get_element_attribute(driver,
                      css_selector: str,
                      get_href: int = 0,
@@ -54,17 +28,6 @@ def get_element_attribute(driver,
         except BaseException as e:
             print(f'Возникла ошибка <{e}>')
             return ""
-        # print(f'Найденный Вэб-Элемент с именем <{found_element}>')
-        # found_href = found_element.get_attribute('href')
-        # if found_href is not None:
-        #     return found_href
-        # try:
-        #     found_elements = driver.find_elements(By.CSS_SELECTOR, css_selector)
-        #     return [i.get_attribute('href') for i in found_elements]
-        # except BaseException as e:
-        #     print(f'Возникла ошибка <{e}>')
-        #     print('По всей видимости данный элемент - не итерируемый!')
-        #     return ""
     return found_elem_text.replace("\n", ", ")
 
 
